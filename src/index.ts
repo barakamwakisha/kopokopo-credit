b59118f41598cde5ec5ecc37756f74522953188a
Baraka Mwakisha <barakamwakisha@gmail.com>
2021-12-11 18:33:44 +0300

    Add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
-import { getTransactions, rankCustomersByCreditScore } from "./domain/credit-score";
+#!/usr/bin/env node
 
-async function main() {
+import {
+  getTransactions,
+  rankCustomersByCreditScore,
+} from "./domain/credit-score";
+
+const argv = require("yargs/yargs")(process.argv.slice(2))
+  .option("csv", {
+    alias: "c",
+    describe: "The path to the csv file with KopoKopo transactions",
+    type: "string"
+  })
+  .option("limit", {
+    alias: "l",
+    describe: "The first n best customers to display",
+    type: "number"
+  })
+  .demandOption("csv", "Please specify the path to the csv file")
+  .demandOption("limit", "Please specify the number of customers to display")
+  .help().argv;
+
+(async () => {
     try {
-        const csvPath = process.argv[2];
-        const limit = parseInt(process.argv[3]);
-
-        console.log(csvPath, limit);
-
-        const transactions = await getTransactions(csvPath);
-        const bestCustomers = await rankCustomersByCreditScore(transactions, limit);
+        const transactions = await getTransactions(argv.csv);
+        const bestCustomers = await rankCustomersByCreditScore(transactions, argv.limit);
 
         console.log(`Best customers are ${JSON.stringify(bestCustomers)}`);
-
         process.exit(0);
-    
-    } catch(ex) {
-        console.error(ex);
-        process.exit(1);
+    } catch (ex) {
+      console.error(ex);
+      process.exit(1);
     }
-}
-
-main();
+})();
